1afa975eb32a746a80f0fff1137fec31b13a7c07
Davian Patino <patinodavian@gmail.com>
2023-03-14 15:32:37 -0500

    First enemy added

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
+import  random
 
-#initialize Pygame
+# initialize Pygame
 pygame.init()
 
-#Set Screen
-screen = pygame.display.set_mode((800, 400))
+# Set Screen
+width = 800
+height = 400
+screen = pygame.display.set_mode((width, height))
 
-#Window Display Settings
+# Window Display Settings
 
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
-#Player
+# Player
 
 PlayerImage = pygame.image.load("spacecraft.png")
 PlayerX = 370
@@ -20,11 +23,23 @@ PlayerY = 320
 Xchange = 0
 Ychange = 0
 
-def Player(x, y):
+# Enemy 1
 
+Enemy_1_Image = pygame.image.load("EnemyAlien.png")
+Enemy_1_X = 370
+Enemy_1_Y = 15
+Enemy_1_Xchange = 0
+Enemy_1_Ychange = 0
+
+
+def Player(x, y):
     screen.blit(PlayerImage, (x, y))
 
-#Game Loop
+def Enemy(x, y):
+    screen.blit(Enemy_1_Image, (x, y))
+
+
+# Game Loop
 
 windowRunning = True
 
@@ -53,6 +68,13 @@ while windowRunning:
                 Ychange = 0
 
     PlayerX += Xchange
+
+    if PlayerX <= 0:
+        PlayerX = 0
+    elif PlayerX >= 736:
+        PlayerX = 736
+
     PlayerY += Ychange
+    Enemy(Enemy_1_X, Enemy_1_Y)
     Player(PlayerX, PlayerY)
     pygame.display.update()
