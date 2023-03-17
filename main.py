2e4f46da23a893f9a1169cb5701a9bc7462ad92c
Davian Patino <patinodavian@gmail.com>
2023-03-17 10:15:10 -0500

    Collision detection.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-import  random
+import random
+import math
 
 # initialize Pygame
 pygame.init()
@@ -26,10 +27,19 @@ Ychange = 0
 # Enemy 1
 
 Enemy_1_Image = pygame.image.load("EnemyAlien.png")
-Enemy_1_X = random.randint(0, 800)
+Enemy_1_X = random.randint(0, 735)
 Enemy_1_Y = random.randint(15, 100)
 Enemy_1_Xchange = 0.1
-Enemy_1_Ychange = 40
+Enemy_1_Ychange = 25
+
+# Bullet
+
+BulletImage = pygame.image.load("missile.png")
+BulletX = 0
+BulletY = 320
+BulletXchange = 0
+BulletYchange = .5
+BulletState = "ready"
 
 # Background
 
@@ -39,8 +49,23 @@ background = pygame.image.load("SpaceBackground.jpeg")
 def Player(x, y):
     screen.blit(PlayerImage, (x, y))
 
+
 def Enemy(x, y):
     screen.blit(Enemy_1_Image, (x, y))
+
+
+def Fire_Bullet(x, y):
+    global BulletState
+    BulletState = "fire"
+    screen.blit(BulletImage, (x + 16, y + 10))
+
+
+def isCollision(Enemy_1_X, BulletX, Enemy_1_Y, BulletY):
+    distance = math.sqrt(math.pow(Enemy_1_X - BulletX, 2) + math.pow(Enemy_1_Y - BulletY, 2))
+    if distance < 27:
+        return True
+    else:
+        return False
 
 
 # Game Loop
@@ -60,6 +85,12 @@ while windowRunning:
                 Xchange = -.1
             if event.key == pygame.K_RIGHT:
                 Xchange = .1
+
+            # Firing the bullet
+            if event.key == pygame.K_SPACE:
+                if BulletState == "ready":
+                    BulletX = PlayerX
+                    Fire_Bullet(BulletX, BulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -84,6 +115,23 @@ while windowRunning:
         PlayerX = 0
     elif PlayerX >= 736:
         PlayerX = 736
+
+    # Bullet Movement
+    if BulletY <= 0:
+        BulletY = 320
+        BulletState = "ready"
+
+    if BulletState == "fire":
+        Fire_Bullet(BulletX, BulletY)
+        BulletY -= BulletYchange
+
+    # Collision Detection
+    collision = isCollision(Enemy_1_X, BulletX, Enemy_1_Y, BulletY)
+    if collision:
+        BulletY = 320
+        BulletState = "ready"
+        Enemy_1_X = random.randint(0, 735)
+        Enemy_1_Y = random.randint(15, 100)
 
     Enemy(Enemy_1_X, Enemy_1_Y)
     Player(PlayerX, PlayerY)
